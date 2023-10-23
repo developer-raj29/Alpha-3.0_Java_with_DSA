18209a481561932f11b5437d0e4aba042e2fe0a7
Raj Yadav <rajyadav7047@gmail.com>
2023-10-24 00:46:08 +0530

    newcommit

--- 04 Advance DSA/27 Queue/_04Queue_Using_JCF.java
+++ 04 Advance DSA/27 Queue/_04Queue_Using_JCF.java
@@ -3,7 +3,7 @@ import java.util.ArrayDeque;// ArrayDeque package import
 // import java.util.Queue; // Queue package import
 import java.util.*; // ALL package import
 
-public class _04_03Queue_Using_JCF {
+public class _04Queue_Using_JCF {
     public static void main(String[] args) {
         // Queue<Integer> q = new LinkedList<>();
         Queue<Integer> q = new ArrayDeque<>();
