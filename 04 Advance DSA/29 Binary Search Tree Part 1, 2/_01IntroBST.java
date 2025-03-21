31742a846899b45096f78e7cf464137df48e4a03
Raj Yadav <rajyadav7047@gmail.com>
2025-03-21 22:34:01 +0530

    commit 2

--- 04 Advance DSA/29 Binary Search Tree Part 1, 2/_01IntroBST.java
+++ 04 Advance DSA/29 Binary Search Tree Part 1, 2/_01IntroBST.java
@@ -1,3 +1,52 @@
-public class _01IntroBST {
 
+public class _01IntroBST {
+    // Build Search a BST
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static Node insert(Node root, int val) {
+        if (root == null) {
+            root = new Node(val);
+            return root;
+        }
+
+        if (root.data > val) {
+            // left subtree
+            root.left = insert(root.left, val);
+        } else {
+            // right subtree
+            root.right = insert(root.right, val);
+        }
+
+        return root;
+    }
+
+    public static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+        int value[] = { 5, 1, 3, 4, 2, 7 };
+        Node root = null;
+
+        for (int i = 0; i < value.length; i++) {
+            root = insert(root, value[i]);
+        }
+
+        inorder(root);
+        System.out.println();
+    }
 }
